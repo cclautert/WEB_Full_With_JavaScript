6a34762d427f0d00203c56e4b840fabc799cfbe2
Cristiano Claudson Lautert <cristiano.c.lautert@gmail.com>
2020-06-29 20:25:36 -0300

    [FIRST-COMMIT] Projeto do curso Desenvolvimento Web Moderno com JavaScript! COMPLETO 2020 + Projetos

---
+++ exercicios-js/Fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+// Comentários de 1 Linha
+console.log("Linha 1");
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console.log("Linha 2");
+
+/*
+ *Comentário
+ *de
+ *múlltiplas
+ *linhas
+ */
---
+++ exercicios-js/Fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+console.log("Sentença de Código");
+
+{
+  {
+    console.log("Teste");
+    console.log("Teste 2");
+  }
+}
